ed3e6acd64ac728005f9850070b4604f42015651
Hamid Ebadi <hamid.ebadi@gmail.com>
2024-04-09 12:44:05 +0200

    adjust build path

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pkill -9 -f gazebo
 pkill -9 -f rviz
 pkill -9 -f humble
 
-colcon build --merge-install --symlink-install --cmake-args "-Wno-dev"  --cmake-args " -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+colcon build --merge-install --symlink-install --cmake-args " -Wno-dev"  --cmake-args " -DCMAKE_BUILD_TYPE=RelWithDebInfo"
